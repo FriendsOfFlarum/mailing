4af143829da25da488aac2eaa31932afa9f6679c
IanM <ian@morland.me>
2026-04-21 12:20:47 +0100

    chore: add unit

---
+++ tests/unit/Jobs/SendMailTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of fof/mailing.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FoF\Mailing\Tests\unit\Jobs;
+
+use FoF\Mailing\Jobs\SendMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use PHPUnit\Framework\TestCase;
+
+class SendMailTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function job_implements_should_queue()
+    {
+        $job = new SendMail('user@example.com', 'Subject', 'Body');
+
+        $this->assertInstanceOf(ShouldQueue::class, $job);
+    }
+
+    /**
+     * @test
+     */
+    public function job_accepts_html_flag()
+    {
+        $job = new SendMail('user@example.com', 'Subject', '<p>Body</p>', true);
+
+        $this->assertInstanceOf(SendMail::class, $job);
+    }
+}
